56d332ad78648544005056d06778d93b83ba991d
ruben <ruben@RUBENMSI>
2022-01-16 20:30:50 -0700

    Add project files.

---
+++ RestaurantTenTablesReservationSystem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * 1. Validar si es usuario existente o si se debe registrar
+ * 2. El programa debe ser capaz de darle la bienbenida a un usuario existente si en efecto existe
+ * 3. El programa debe repetirse hasta que se registren las 10 personas
+*/
+namespace RestaurantTenTablesReservationSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
