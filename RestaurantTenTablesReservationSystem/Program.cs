9391b13249a33fada905eea3713e9e7375d0c1bb
Jesús Rubén Esparza Soto <rubenesparzasoto95@gmail.com>
2022-01-16 20:55:23 -0700

    Introducción del proyecto

--- RestaurantTenTablesReservationSystem/Program.cs
+++ RestaurantTenTablesReservationSystem/Program.cs
@@ -1,17 +1,34 @@
 ﻿using System;
 
 /*
- * 1. Validar si es usuario existente o si se debe registrar
- * 2. El programa debe ser capaz de darle la bienbenida a un usuario existente si en efecto existe
- * 3. El programa debe repetirse hasta que se registren las 10 personas
+ * 1. Validar si es usuario existente o si se debe registrar, y generar un sistema de registro o login.
+ * 2. El programa debe ser capaz de darle la bienbenida a un usuario existente si en efecto existe.
+ * 3. El programa debe repetirse hasta que se registren las 10 personas.
 */
 namespace RestaurantTenTablesReservationSystem
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            string[] username = new string[10] { "Mike", "", "", "", "", "", "", "", "", "" };
+            int arrayCurrentIndex = 1;
+            bool userType = false;
+
+            Console.WriteLine("Welcome to the best restaurant in the world!");
+
+            while (true)
+            {
+                Console.WriteLine("Are you a registered user? Write true, or write false to register");
+                userType = Convert.ToBoolean(Console.ReadLine());
+
+                if (userType)
+                {
+                    Console.WriteLine("Hello, you are a registered user, please enter your exact user name");
+                    string userToSearch = Console.ReadLine();
+                    Console.WriteLine($"The user you searched is { userToSearch }"); 
+                }
+            }
         }
     }
 }
